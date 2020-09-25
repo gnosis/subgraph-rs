79398a20a74bdaeb47059b1dd47c3cadcd01037b
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-09-25 15:26:52 +0200

    implement abort panic hook (#8)
    This PR implements a Rust panic hook that call's the host's runtime `abort` method.
    
    ### Test Plan
    
    Like #7 this can't really be tested without a sample subgraph that will come in a later PR.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod abort;
 mod ffi;
 mod logger;
 
@@ -6,13 +7,14 @@ pub use log;
 /// Module containing required Wasm exports.
 #[doc(hidden)]
 pub mod exports {
-    use crate::logger;
+    use crate::{abort, logger};
 
     /// The Wasm start function. This gets set as the module's start function
     /// during post-processing of the Wasm blob, since there is currently no
     /// way to specify it in Rust at the moment.
     #[export_name = "__subgraph_start"]
     pub extern "C" fn start() {
+        abort::set_panic_hook();
         logger::init();
     }
 }
