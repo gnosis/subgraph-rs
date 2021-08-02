e80a0662a912bd464328c17ac2f63cad8d3a5530
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-08-02 20:18:15 +0200

    Introduce sys module that can be mocked (#14)
    This PR introduces some conditional compilation so we can mock the Graph host functions for unit testing.

---
+++ src/sys/mock.rs
@@ -0,0 +1,34 @@
+//! Mock host import function bindings.
+
+#![allow(non_snake_case)]
+
+use super::BigInt;
+use crate::ffi::string::AscStr;
+
+pub unsafe fn abort(_: &AscStr, _: Option<&AscStr>, _: u32, _: u32) -> ! {
+    unreachable!("mocked abort host method called");
+}
+
+pub mod bigInt {
+    use super::*;
+
+    pub unsafe fn plus<'host>(_x: BigInt, _y: BigInt) -> BigInt<'host> {
+        todo!()
+    }
+}
+
+pub mod log {
+    use super::*;
+
+    pub unsafe fn log(_: u32, _: &AscStr) {
+        unreachable!("mocked log host method called");
+    }
+}
+
+pub mod typeConversion {
+    use super::*;
+
+    pub unsafe fn bigIntToString(_x: BigInt) -> &AscStr {
+        todo!()
+    }
+}
